be5f6430811f18c8b493b29d2ec332d80954b5bd
Arleigh.Li <suncms@qq.com>
2014-12-15 21:45:45 +0800

    list 标签解析器

---
+++ SunCode/sun.generating/MakeList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace sun.generating
+{
+    /// <summary>
+    /// // List 标签解析
+    /// </summary>
+    class MakeList
+    {
+    }
+}
